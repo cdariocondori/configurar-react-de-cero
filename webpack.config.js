05e37f5753278a2013692d56cfd2e4d3eed0ba43
dariodev <dario@@gmail.com>
2021-11-03 16:06:36 -0300

    configurando webpack loaders css sass html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -12,17 +15,43 @@ module.exports = {
         //configurando babel
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.(js|jsx)$/,//indentificar js y jsx
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.html$/,//indentificad html
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/,// identificar sass o css
+                use: [//elementos a usar
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     },
+    plugins: [// plugins instanciados en la parte superior 
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
+        static: path.join(__dirname, 'dist'),
         compress: true,
+        open: true,
         port: 3006
     }
 }
