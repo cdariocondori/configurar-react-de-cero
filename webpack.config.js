cbb2955b95a68412d778c566aff5e83a2ab4703e
dariodev <dario@@gmail.com>
2021-11-03 09:28:45 -0300

    configuracion base de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,28 @@
-c
+const path = require('path')
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        //configurando babel
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3006
+    }
+}
